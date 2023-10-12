56ac390f5478d28170a02903d145c5239729d91b
Harris Rothaermel <harris.rothaermel@gmail.com>
2023-10-11 17:28:09 -0700

    Swap to Cloudflare AI Gateway

--- src/services/openai.ts
+++ src/services/openai.ts
@@ -2,7 +2,10 @@ import OpenAI from 'openai';
 
 // Documentation: https://platform.openai.com/docs/introduction
 
-const openai = new OpenAI();
+const openai = new OpenAI({
+  baseURL:
+    'https://gateway.ai.cloudflare.com/v1/c38bc3761ea690e1a45693a40ce4fb2f/bonfire/openai',
+});
 
 export type Message = {
   role: 'system' | 'assistant' | 'user' | 'function';
